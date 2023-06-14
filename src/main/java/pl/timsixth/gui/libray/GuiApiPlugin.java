581f85cdcae031cc64ab1bcc001db1d3d4a4b235
timsixth <timsixth@op.pl>
2023-06-14 19:43:43 +0200

    Deleted static method to get API

--- src/main/java/pl/timsixth/gui/libray/GuiApiPlugin.java
+++ src/main/java/pl/timsixth/gui/libray/GuiApiPlugin.java
@@ -1,10 +1,7 @@
 package pl.timsixth.gui.libray;
 
-import lombok.AccessLevel;
-import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import pl.timsixth.gui.libray.api.GuiApi;
 import pl.timsixth.gui.libray.example.ConfigFile;
 import pl.timsixth.gui.libray.example.MenuManager;
 import pl.timsixth.gui.libray.example.TestCommand;
@@ -18,26 +15,15 @@ import pl.timsixth.gui.libray.model.action.custom.impl.SendMessageAction;
 
 public final class GuiApiPlugin extends JavaPlugin {
 
-    @Getter(AccessLevel.PACKAGE)
-    private ActionRegistration actionRegistration;
-
-    private static GuiApi guiApi;
-
     @Override
     public void onEnable() {
-        guiApi = new GuiApiImpl(this);
-
         ConfigFile configFile = new ConfigFile(this);
-        actionRegistration = new ActionRegistrationImpl();
+        ActionRegistration actionRegistration = new ActionRegistrationImpl();
         actionRegistration.register(new NoneClickAction(), new SendMessageAction(), new GiveItemsActionImpl());
         AbstractMenuManager menuManager = new MenuManager(actionRegistration, configFile);
         menuManager.load();
         Bukkit.getPluginManager().registerEvents(new InventoryClickListener(menuManager), this);
 
         getCommand("test").setExecutor(new TestCommand(menuManager));
-    }
-
-    public static GuiApi getApi() {
-        return guiApi;
     }
 }
--- src/main/java/pl/timsixth/gui/libray/api/GuiApi.java
+++
@@ -1,8 +0,0 @@
-package pl.timsixth.gui.libray.api;
-
-import pl.timsixth.gui.libray.manager.registration.ActionRegistration;
-
-public interface GuiApi {
-
-    ActionRegistration getActionRegistration();
-}
